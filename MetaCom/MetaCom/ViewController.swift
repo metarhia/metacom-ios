cb23d7c4015bec05729e686663eac2f28870145b
GreatAndPowerfulKing <pedos.vm@icloud.com>
2017-05-18 13:47:43 +0300

    Create project

---
+++ MetaCom/MetaCom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetaCom
+//
+//  Created by iKing on 11.05.17.
+//  Copyright © 2017 iKing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
+
