3dc0f7af2e369f548f595675c57d272a461c0079
GYFK <notthewhite@gmail.com>
2017-05-21 11:27:24 +0300

    rename chat to chatroom

---
+++ MetaCom-iOS/Core/Models/ChatRoom.swift
@@ -0,0 +1,15 @@
+//
+//  ChatRoom.swift
+//  MetaCom-iOS
+//
+//  Created by Artem Chernenkiy on 17.05.17.
+//  Copyright © 2017 Metarhia. All rights reserved.
+//
+
+import Foundation
+import JSTP
+
+/**
+	A type representing a chat conversation.
+*/
+class ChatRoom {
---
+++ MetaCom-iOS/Core/Models/ChatRoomManager.swift
@@ -0,0 +1,14 @@
+//
+//  ChatRoomManager.swift
+//  MetaCom-iOS
+//
+//  Created by Artem Chernenkiy on 17.05.17.
+//  Copyright © 2017 Metarhia. All rights reserved.
+//
+
+import Foundation
+
+/**
+	A type representing a chat conversation manager.
+*/
+final class ChatRoomManager {
