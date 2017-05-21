01818c411acea3c9cc4dd67ab5fbd01fb1c411fa
GYFK <notthewhite@gmail.com>
2017-05-21 11:29:16 +0300

    add constants for events, notifications, interfaces

---
+++ MetaCom-iOS/Extensions/NotificationName+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationName+Extensions.swift
+//  MetaCom-iOS
+//
+//  Created by Artem Chernenkiy on 20.05.17.
+//  Copyright © 2017 Metarhia. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+	
+	static let MCConnectionEstablished = Notification.Name(rawValue: "metacom.connection.wasEstablished")
+	static let MCConnectionDidReceive = Notification.Name(rawValue: "metacom.connection.didReceiveData")
+	static let MCConnectionLost = Notification.Name(rawValue: "metacom.connection.wasLost")
+}
