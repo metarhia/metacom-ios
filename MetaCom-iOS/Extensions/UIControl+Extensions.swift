b8ef1d965b369d1de33d3c272dcb7e12f3715344
GYFK <notthewhite@gmail.com>
2017-07-24 18:27:26 +0300

    add UIControl addAction with swift closure

---
+++ MetaCom-iOS/Extensions/UIControl+Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  UIControl+Extensions.swift
+//  MetaCom-iOS
+//
+//  Created by Artem Chernenkiy on 09.07.17.
+//  Copyright © 2017 Metarhia. All rights reserved.
+//
+
+import UIKit
+
+extension UIControl {
+  
+  private class ClosureWrapper {
+    
+    let closure: () -> ()
+    
+    init(owner: AnyObject, block: @escaping () -> ()) {
+      closure = block
+      objc_setAssociatedObject(owner, "\(owner.description)\(arc4random())", self, .OBJC_ASSOCIATION_RETAIN)
+    }
+    
+    @objc func run() {
+      closure()
+    }
+  }
+  
+  func addAction(for controlEvents: UIControlEvents, action: @escaping () -> ()) {
+    let wrapper = ClosureWrapper(owner: self, block: action)
+    addTarget(wrapper, action: #selector(ClosureWrapper.run), for: controlEvents)
+  }
+}
