b67a43be756fa0bd9d7c381d218e664b86647ef1
GYFK <notthewhite@gmail.com>
2017-07-24 18:25:54 +0300

    add localizable

---
+++ MetaCom-iOS/Extensions/String+Localized.swift
@@ -0,0 +1,20 @@
+//
+//  String+Localized.swift
+//  MetaCom-iOS
+//
+//  Created by Artem Chernenkiy on 07.07.17.
+//  Copyright © 2017 Metarhia. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+  
+  func localized(in bundle: Bundle = Bundle.main, table: String?) -> String {
+    return bundle.localizedString(forKey: self, value: self, table: table)
+  }
+  
+  var localized: String {
+    return localized(table: "Localizable")
+  }
+}
