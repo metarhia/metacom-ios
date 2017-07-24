a0bd26dc8e6c4e6860901460687726133f544a3c
GYFK <notthewhite@gmail.com>
2017-07-24 18:33:01 +0300

    add the ability to extract mime type and vice versa

--- MetaCom-iOS/Core/Models/FileManager.swift
+++ MetaCom-iOS/Core/Models/FileManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import MobileCoreServices
 
 /**
 	A type representing a user connection file manager.
@@ -43,6 +44,57 @@ final class FileManager {
 	func download(_ code: String, _ completion: (Data?, Error?) -> Void) {
 		
 	}
+extension FileManager {
+  
+  /**
+    Construct a mime type from the uniform type identifier.
+   	- parameters:
+   		- uti: uniform type identifier.
+   	- returns: mime type for the specfied uniform type identifier.
+   */
+  class func extractMimeType(from uti: String?) -> String {
+    
+    guard let type = uti, let mime = UTTypeCopyPreferredTagWithClass(type as CFString, kUTTagClassMIMEType) else {
+      return extractMimeType(from: String(kUTTypeData))
+    }
+    
+    return mime.takeRetainedValue() as String
+  }
+  
+  /**
+   	Construct a file mime type from the url.
+    - parameters:
+   		- url: url of the file.
+   	- returns: mime type of the file specified by url.
+   */
+  class func extractMimeType(from url: URL) -> String {
+    
+    let pathExtension = url.pathExtension as NSString
+    guard let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension, nil) else {
+      return extractMimeType(from: String(kUTTypeData))
+    }
+    
+    return extractMimeType(from: String(uti.takeRetainedValue()))
+  }
+  
+  /**
+   	Construct a file extension from the given mime type.
+    - parameters:
+    	- mimeType: mime type represented in string.
+   	- returns: file extension for the specified mime type or an empty string.
+   */
+  class func extractExtension(from mimeType: String) -> String {
+    
+    let emptyString = "" as CFString
+    
+    let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, mimeType as CFString, nil)
+    let utiValue = uti?.takeRetainedValue() ?? emptyString
+    let fileExtension = UTTypeCopyPreferredTagWithClass(utiValue, kUTTagClassFilenameExtension)
+    
+    return String(fileExtension?.takeRetainedValue() ?? emptyString)
+  }
+}
+
 extension Constants {
 	
 	/// Chunk Size currently has to be < 4 mb.
